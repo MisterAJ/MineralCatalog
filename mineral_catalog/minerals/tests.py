fddcfe16ad59e5edf0ba51176b1989550bfafd6e
MisterAJ <ajlongstreet@gmail.com>
2018-01-10 21:15:59 -0500

    adds tests

--- mineral_catalog/minerals/tests.py
+++ mineral_catalog/minerals/tests.py
@@ -29,3 +29,21 @@ class MineralViewsTests(TestCase):
         resp = self.client.get('/')
         self.assertEqual(resp.status_code, 200)
         self.assertContains(resp, self.mineral.name)
+
+class MineralFilterTests(TestCase):
+    def setUp(self):
+        self.mineral = Mineral.objects.create(
+            name='Granite'
+        )
+
+    def test_category_filter(self):
+        resp = self.client.get('/category/sulfate/')
+        self.assertEqual(resp.status_code, 200)
+
+    def test_name_filter(self):
+        resp = self.client.get('/startswith/g/')
+        self.assertEqual(resp.status_code, 200)
+
+    def test_search(self):
+        resp = self.client.post('/search/', {'search':'nyancats'})
+        self.assertEqual(resp.status_code, 200)
